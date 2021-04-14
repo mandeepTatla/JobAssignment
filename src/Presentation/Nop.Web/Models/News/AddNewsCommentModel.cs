4d19ae473fd1d1c09076b8a2c11e2c17717d29be
Mandeep Tatla <mandeeptatla2808@gmail.com>
2021-04-14 14:33:29 +0930

    Added all files

---
+++ src/Presentation/Nop.Web/Models/News/AddNewsCommentModel.cs
@@ -0,0 +1,19 @@
+﻿using Nop.Web.Framework.Mvc.ModelBinding;
+using Nop.Web.Framework.Models;
+
+namespace Nop.Web.Models.News
+{
+    public partial record AddNewsCommentModel : BaseNopModel
+    {
+        [NopResourceDisplayName("News.Comments.CommentTitle")]
+        public string CommentTitle { get; set; }
+
+        [NopResourceDisplayName("News.Comments.CommentText")]
+        public string CommentText { get; set; }
+
+        //Default = false
+        public bool AutomaticallyApproveComments { get => true; set => AutomaticallyApproveComments = value; }
+      
+        public bool DisplayCaptcha { get; set; }
+    }
+}
